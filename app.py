420b0bbfa3a4de45fd4065d15b4cbc8076937d21
wyattbrashear <bfamily610@icloud.com>
2026-03-19 21:41:43 -0400

    Add 1st testing version

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import json
 import os
 import sqlite3
 import threading
+import time
+from datetime import datetime
 
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, request, render_template
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def _init_db():
@@ -12,11 +18,67 @@ def _init_db():
     db.commit()
 app = Flask(__name__)
 
+def exec_crawl(url, depth):
+    db = sqlite3.connect('SearchData.db')
+    db.execute('CREATE TABLE IF NOT EXISTS SearchData (url TEXT, title TEXT, content TEXT, datetime TEXT, idfdf TEXT)')
+    db.execute("CREATE VIRTUAL TABLE IF NOT EXISTS SearchData_index USING fts5(url, title, content)")
+    db.commit()
+    depth = int(depth)
+    headers = {'User-Agent': "SmoogleBot"}
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    documents = [str(soup.get_text())]
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(documents)
+    json_data = {
+        "wordlist": vectorizer.get_feature_names_out().tolist()
+    }
+    try:
+        title = soup.title.text
+    except:
+        title = "None"
+    db.execute('INSERT INTO SearchData (url, title, content, datetime, idfdf) VALUES (?, ?, ?, ?, ?)',
+               (url, title, str(soup.get_text()), datetime.now().isoformat(), json.dumps(json_data)))
+    db.execute('INSERT INTO SearchData_index (url, title, content) VALUES (?, ?, ?)',
+               (url, title, str(soup.get_text()),))
+    links = soup.find_all('a')
+    db.commit()
+    if depth > 0:
+        for link in links:
+            if link.get('href') is not None:
+                if link.get('href').startswith('http') and link.get('href') not in db.execute(
+                        'SELECT url FROM SearchData').fetchall():
+                    try:
+                        r = requests.get(link['href'], headers=headers)
+                        soup = BeautifulSoup(r.text, 'html.parser')
+                        documents = [str(soup.get_text())]
+                        vectorizer = TfidfVectorizer()
+                        tfidf_matrix = vectorizer.fit_transform(documents)
+                        json_data = {
+                            "wordlist": vectorizer.get_feature_names_out().tolist()
+                        }
+                        try:
+                            title = soup.title.text
+                        except Exception as e:
+                            print(e)
+                            title = "None"
+                        db.execute(
+                            'INSERT INTO SearchData (url, title, content, datetime, idfdf) VALUES (?, ?, ?, ?, ?)',
+                            (link['href'], title, str(soup.get_text()), datetime.now().isoformat(),
+                             json.dumps(json_data)))
+                        db.execute('INSERT INTO SearchData_index (url, title, content) VALUES (?, ?, ?)',
+                                   (link['href'], title, str(soup.get_text()),))
+                        db.commit()
+                    except Exception as e:
+                        print(e)
+                    time.sleep(5)
+        db.commit()
+
 @app.route("/crawl", methods=["POST"])
 def crawl():
     url = request.form["url"]
     depth = int(request.form["depth"])
-    threading.Thread(target=lambda: os.system(f"python SmoogleCrawler.py {url} --depth {depth}")).start()
+    threading.Thread(target=lambda: exec_crawl(url, depth)).start()
     return 'Crawling Started, <a href="/">Go Home</a>', 202
 @app.route("/crawl-web/", methods=["GET"])
 def crawl_get():
